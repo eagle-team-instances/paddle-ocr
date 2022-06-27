aaf0f7be04cb0bc41992aa407123901b2a708649
ubuntu <ubuntu@localhost.localdomain>
2022-06-28 00:11:36 +0800

    feat: add font

--- ocr.py
+++ ocr.py
@@ -10,12 +10,14 @@ for line in result:
 
 # 显示结果
 from PIL import Image
+from PIL import ImageFont
+font = ImageFont.load_default()
 
 image = Image.open(img_path).convert('RGB')
 boxes = [line[0] for line in result]
 txts = [line[1][0] for line in result]
 scores = [line[1][1] for line in result]
-# im_show = draw_ocr(image, boxes, txts, scores, font_path='./fonts/simfang.ttf')
-im_show = draw_ocr(image, boxes, txts, scores)
+im_show = draw_ocr(image, boxes, txts, scores, font_path='Roboto-Light.ttf')
+# im_show = draw_ocr(image, boxes, txts, scores, font_path=font)
 im_show = Image.fromarray(im_show)
 im_show.save('result.jpg')
